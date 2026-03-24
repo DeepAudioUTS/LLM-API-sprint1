115ff19d95df8a18bcf1c07511b477da2a39e26e
harunori-kawano <k.hnori0507@gmail.com>
2026-03-24 23:26:13 +1100

    update

--- story_api/main.py
+++ story_api/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from story_api.routes.v1 import api_v1_router
 
 
 app = FastAPI(title="Story Generation API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_v1_router, prefix="/api")
 
 
